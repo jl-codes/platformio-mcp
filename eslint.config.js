dfd93fa6f118d27034808b76efe7d0a672f51a31
A-steeper <aeisenhawer@steeperenergy.com>
2026-04-11 13:20:05 -0600

    updating dependencies and fixing vulnerabilities

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['build/**', 'node_modules/**'],
+  }
+);
